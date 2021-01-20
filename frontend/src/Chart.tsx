829158188d90e862d689330bcb0cb3a6b2484411
Noahf99 <fourniernoah6@gmail.com>
2021-01-20 17:14:00 +0000

    Add search form and mock data

--- frontend/src/Chart.tsx
+++ frontend/src/Chart.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
+import { ResponsiveLine } from "@nivo/line";
+import { Container } from "@material-ui/core";
 
 const Chart = () => {
-  return <p>Chart</p>;
+  return (
+    <div>
+      <Container maxWidth="lg">CHART GOES HERE</Container>
+    </div>
+  );
 };
 
 export default Chart;
--- frontend/src/SearchBar.tsx
+++ frontend/src/SearchBar.tsx
@@ -1,8 +1,8 @@
 import {
   AppBar,
+  Button,
   createStyles,
   fade,
-  FormControl,
   FormControlLabel,
   InputBase,
   makeStyles,
@@ -11,6 +11,7 @@ import {
   Toolbar,
 } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/SearchOutlined";
+import { Form, Formik } from "formik";
 import * as React from "react";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -28,7 +29,6 @@ const useStyles = makeStyles((theme: Theme) =>
       position: "relative",
       marginLeft: 0,
       marginRight: theme.spacing(2),
-      width: "40%",
       backgroundColor: fade(theme.palette.common.black, 0.15),
       "&:hover": {
         backgroundColor: fade(theme.palette.common.black, 0.25),
@@ -59,35 +59,63 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+interface Values {
+  stock: string;
+}
+
 interface Props {
+  onSubmit: React.Dispatch<React.SetStateAction<string>>;
   isDarkMode: boolean;
   setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const SearchBar = ({ isDarkMode, setDarkMode }: Props) => {
+const SearchBar = ({ onSubmit, isDarkMode, setDarkMode }: Props) => {
   const classes = useStyles();
+
   return (
     <div className={classes.grow}>
       <AppBar position="static" className={classes.appBar}>
         <Toolbar className={classes.toolbar}>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <FormControl>
-              <InputBase
-                placeholder="Enter Stock Symbol..."
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-              />
-            </FormControl>
-          </div>
+          <Formik
+            initialValues={{ stock: "" }}
+            onSubmit={(values, { resetForm }) => {
+              onSubmit(values.stock);
+              resetForm({});
+            }}
+          >
+            {({ values, handleChange }) => (
+              <Form style={{ display: "flex" }}>
+                <div className={classes.search}>
+                  <div className={classes.searchIcon}>
+                    <SearchIcon />
+                  </div>
+                  <InputBase
+                    name="stock"
+                    onChange={handleChange}
+                    value={values.stock}
+                    type="text"
+                    placeholder="Enter Stock Symbol..."
+                    classes={{
+                      root: classes.inputRoot,
+                      input: classes.inputInput,
+                    }}
+                  />
+                </div>
+                <Button variant="contained" type="submit">
+                  Search
+                </Button>
+              </Form>
+            )}
+          </Formik>
           <div className={classes.modeToggle}>
             <FormControlLabel
               label={"Toggle Dark Mode"}
-              control={<Switch onClick={() => setDarkMode(!isDarkMode)} />}
+              control={
+                <Switch
+                  checked={isDarkMode}
+                  onClick={() => setDarkMode(!isDarkMode)}
+                />
+              }
             />
           </div>
         </Toolbar>
