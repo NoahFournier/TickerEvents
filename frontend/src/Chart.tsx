b5834b6a2a99f6c40fa2ae39164f735eb1a36970
Noahf99 <fourniernoah6@gmail.com>
2021-01-20 17:14:51 +0000

    Remove incomplete line chart

--- frontend/src/Chart.tsx
+++ frontend/src/Chart.tsx
@@ -2,10 +2,16 @@ import * as React from "react";
 import { ResponsiveLine } from "@nivo/line";
 import { Container } from "@material-ui/core";
 
+interface Props {
+  data: {};
+}
+
 const Chart = () => {
   return (
     <div>
-      <Container maxWidth="lg">CHART GOES HERE</Container>
+      <Container maxWidth="lg">
+        <div>CHART GOES HERE</div>
+      </Container>
     </div>
   );
 };
